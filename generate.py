0bb37d40fc28d1f747019fc37a28b91fd0564c5b
alessio <gravina.alessio@gmail.com>
2021-11-25 11:53:29 +0100

    stochastic_block_model

--- generate.py
+++ generate.py
@@ -1,16 +1,28 @@
 import numpy as np
 from utils import remove_self_loops
 
-def erdos_renyi(num_nodes, prob, directed=False, rng=None):
-    r"""Returns the :obj:`edge_index` of a random Erdos-Renyi graph.
 
-    Args:
-        num_nodes (int): The number of nodes.
-        edge_prob (float): Probability of an edge.
-        directed (bool, optional): If set to :obj:`True`, will return a
-            directed graph. (default: :obj:`False`)
+def erdos_renyi_graph(num_nodes, prob, directed=False, rng=None):
     """
+    Returns a random graph, also known as an Erdos-Renyi graph or a binomial graph.
+    The model chooses each of the possible edges with a defined probability.
 
+    Parameters
+    ----------
+    num_nodes : Int
+        The number of nodes
+    prob : Float
+        Probability of an edge
+    directed : Bool, optional
+        If set to True, will return a directed graph, by default False
+    rng : np.random.Generator, optional
+        Numpy random number generator, by default None
+
+    Returns
+    -------
+    np.ndarray
+        The random graph (num_edges x 2)
+    """
     if rng is None:
         rng = np.random.default_rng()
 
@@ -23,17 +35,133 @@ def erdos_renyi(num_nodes, prob, directed=False, rng=None):
 
     return np.argwhere(mask)
 
+
+def stochastic_block_model(block_sizes, probs, generator, rng=None):
+    """
+    Returns a stochastic block model graph.
+    This model partitions the nodes into blocks of defined sizes,
+    and places edges between pairs of nodes depending on a probability matrix.
+    Such a matrix specifies edge probabilities between and inside blocks.
+
+    Parameters
+    ----------
+    block_sizes : List[Int]
+        Sizes of blocks
+    probs : List[List[Float]]
+        The squared probability matrix (num_blocks x num_blocks).
+        The element i,j represents the edge probability between blocks i and j.
+        The element i,i define the edge probability inside block i.
+    generator : Callable
+        A callable that generates communities with size depending on block_sizes
+    rng : np.random.Generator, optional
+        Numpy random number generator, by default None
+
+    Returns
+    -------
+    np.ndarray
+        The stochastic block model graph (num_edges x 2)
+    """
+    if rng is None:
+        rng = np.random.default_rng()
+
+    communities = [generator(b, probs[i], rng=rng) for i, b in enumerate(block_sizes)]
+
+    # Update communities's indices
+    sizes = {}
+    first_id = 0
+    for i in range(len(block_sizes)):
+        communities[i] += first_id
+        sizes[i] = first_id
+        first_id += block_sizes[i]
+
+    # Compute iter-block links
+    edges = []
+    for i in range(len(probs)):
+        for j in range(len(probs)):
+            if i == j: continue
+
+            p = probs[i][j]
+            size_c1, size_c2 = block_sizes[i], block_sizes[j]
+
+            mask = rng.random((size_c1, size_c2)) <= p
+
+            inter_block_edges = np.argwhere(mask)
+            inter_block_edges[:, 0] += sizes[i]
+            inter_block_edges[:, 1] += sizes[j]
+
+            edges.append(inter_block_edges)
+
+    return np.concatenate(edges + communities)
+
+
 def full_grid(height, width):
+    """
+    Returns a full two-dimensional rectangular grid lattice graph.
+    Example
+    1 - 2 - 3
+    | X | X |
+    4 - 5 - 6
+
+    Parameters
+    ----------
+    height : Int
+        Number of vertices in the vertical axis
+    width : Int
+        Number of vertices in the horizontal axis
+
+    Returns
+    -------
+    np.ndarray
+        The full two-dimensional rectangular grid lattice graph (num_edges x 2)
+    """
     w = width
     kernel = np.array([-w - 1, -1, w - 1, -w, w, -w + 1, 1, w + 1])
     return grid(height, width, kernel)
 
+
 def simple_grid(height, width):
+    """
+    Returns a two-dimensional rectangular grid lattice graph.
+    Example
+    1 -- 2 -- 3
+    |    |    |
+    4 -- 5 -- 6
+
+    Parameters
+    ----------
+    height : Int
+        Number of vertices in the vertical axis
+    width : Int
+        Number of vertices in the horizontal axis
+
+    Returns
+    -------
+    np.ndarray
+        The two-dimensional rectangular grid lattice graph (num_edges x 2)
+    """
     w = width
     kernel = np.array([-1, -w, w, 1])
     return grid(height, width, kernel)
 
+
 def grid(height, width, kernel):
+    """
+    Ausiliar function for grid graph generation.
+
+    Parameters
+    ----------
+    height : Int
+        Number of vertices in the vertical axis
+    width : Int
+        Number of vertices in the horizontal axis
+    kernel : List
+        The kernel
+
+    Returns
+    -------
+    np.ndarray
+        The two-dimensional grid lattice graph (num_edges x 2)
+    """
     num_nodes = height * width
     K = len(kernel)
 
--- utils.py
+++ utils.py
@@ -1,6 +1,22 @@
-from numpy.typing import NDArray
+import numpy as np
 
-def to_dense(adj: NDArray, num_nodes=None):
+
+def to_dense(adj, num_nodes=None):
+    """
+    Converts a list of edges in a squared adjacency matrix
+
+    Parameters
+    ----------
+    adj : np.ndarray
+        The list of edges (num_edges x 2)
+    num_nodes : int, optional
+        The number of nodes in the graph, by default None
+
+    Returns
+    -------
+    np.ndarray
+        The squared adjacency matrix (num_nodes x num_nodes)
+    """
     if not num_nodes:
         num_nodes = np.max(adj) + 1
 
@@ -11,14 +27,46 @@ def to_dense(adj: NDArray, num_nodes=None):
 
     return dense_adj
 
+
 def dense(generator):
+    """
+    Compute the squared adjacency matrix of a generated graph
+
+    Parameters
+    ----------
+    generator : Callable
+        A callable that generates the graphs
+
+    Returns
+    -------
+    np.ndarray
+        The squared adjacency matrix (num_nodes x num_nodes)
+    """
     return lambda *args: to_dense(generator(*args))
 
-def remove_self_loops(adj: NDArray):
 
+def remove_self_loops(adj):
+    """
+    Removes every self-loop in the graph given by adj
+
+    Parameters
+    ----------
+    adj : np.ndarray
+        The adjancency matrix
+
+    Returns
+    -------
+    np.ndarray
+        The list of edges without self-loops (num_edges x 2)
+
+    Raises
+    ------
+    NotImplementedError
+    """
     row, col = adj.shape
 
     if row == col:
+        # The case of a squared dense adj matrix
         return adj * (1 - np.eye(row, col, dtype=np.bool8))
 
     raise NotImplementedError()
